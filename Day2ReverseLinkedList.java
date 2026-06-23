aabdd1808fe259f3b28ba7270e97be461d8f51f1
Shashank Shekhar Prakash <shashanksp02@gmail.com>
2026-06-23 23:09:31 +0530

    Day 2 - Reverse Linked List

---
+++ Day2ReverseLinkedList.java
@@ -0,0 +1,51 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class  Day2ReverseLinkedList {
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original Linked List:");
+        printList(head);
+
+        head = reverseList(head);
+
+        System.out.println("Reversed Linked List:");
+        printList(head);
+    }
+}
