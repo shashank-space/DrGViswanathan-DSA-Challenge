493e8f9bd15db9c50b310c83a772eda4ed0df047
Shashank Shekhar Prakash <shashanksp02@gmail.com>
2026-06-28 14:47:31 +0530

    Day 8 - Palindrome

---
+++ Day8PalindromeLinkedList.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Day8PalindromeLinkedList {
+
+    public static boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null;
+        while (slow != null) {
+            ListNode nextNode = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        ListNode first = head;
+        ListNode second = prev;
+
+        while (second != null) {
+            if (first.val != second.val) {
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+
+        if (n == 0) {
+            System.out.println(true);
+            return;
+        }
+
+        System.out.println("Enter node values:");
+        ListNode head = new ListNode(sc.nextInt());
+        ListNode current = head;
+
+        for (int i = 1; i < n; i++) {
+            current.next = new ListNode(sc.nextInt());
+            current = current.next;
+        }
+
+        System.out.println(isPalindrome(head));
+
+        sc.close();
+    }
+}
