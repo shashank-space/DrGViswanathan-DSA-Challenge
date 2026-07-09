613be9b44f9ade5ea8571ce6cd1761d915aa15cc
Shashank Shekhar Prakash <shashanksp02@gmail.com>
2026-07-09 16:30:25 +0530

    Day 17 - Partition List

---
+++ Day17PartitionList.java
@@ -0,0 +1,56 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Day17PartitionList {
+
+    public static ListNode partition(ListNode head, int x) {
+        ListNode lessDummy = new ListNode(0);
+        ListNode greaterDummy = new ListNode(0);
+
+        ListNode less = lessDummy;
+        ListNode greater = greaterDummy;
+
+        while (head != null) {
+            if (head.val < x) {
+                less.next = head;
+                less = less.next;
+            } else {
+                greater.next = head;
+                greater = greater.next;
+            }
+            head = head.next;
+        }
+
+        greater.next = null;
+        less.next = greaterDummy.next;
+
+        return lessDummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(2);
+
+        ListNode result = partition(head, 3);
+
+        printList(result);
+    }
+}
