d4bf2c52220ab1214d2af9364eb4ee2313e79db6
Shashank-space <shashanksp02@gmail.com>
2026-07-17 00:30:11 +0530

    Day 26 - Evaluate Reverse Polish Notation

---
+++ Day26EvaluateReversePolishNotation.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+public class Day26EvaluateReversePolishNotation {
+
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+
+            switch (token) {
+
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+
+                case "-":
+                    int b1 = stack.pop();
+                    int a1 = stack.pop();
+                    stack.push(a1 - b1);
+                    break;
+
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+
+                case "/":
+                    int b2 = stack.pop();
+                    int a2 = stack.pop();
+                    stack.push(a2 / b2);
+                    break;
+
+                default:
+                    stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        String[] tokens = {"2", "1", "+", "3", "*"};
+        System.out.println("Result: " + evalRPN(tokens));
+    }
+}
