8e4d1aadb18efd1f830963224b6e74424b501aa4
Shashank-space <shashanksp02@gmail.com>
2026-07-17 00:28:22 +0530

    Day 25 - Best Time To Buy And Sell Stock

---
+++ Day25BestTimeToBuyAndSellStock.java
@@ -0,0 +1,23 @@
+public class Day25BestTimeToBuyAndSellStock {
+
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        System.out.println("Maximum Profit: " + maxProfit(prices));
+    }
+}
