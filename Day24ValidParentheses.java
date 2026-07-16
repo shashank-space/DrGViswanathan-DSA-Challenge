8872847df6d5b136dd6a4388157773bbc2ba1b72
Shashank-space <shashanksp02@gmail.com>
2026-07-17 00:26:50 +0530

    Day 24 - Valid Parentheses

---
+++ Day24ValidParentheses.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+public class Day24ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s1 = "()";
+        String s2 = "()[]{}";
+        String s3 = "(]";
+        String s4 = "([)]";
+        String s5 = "{[]}";
+
+        System.out.println(isValid(s1)); // true
+        System.out.println(isValid(s2)); // true
+        System.out.println(isValid(s3)); // false
+        System.out.println(isValid(s4)); // false
+        System.out.println(isValid(s5)); // true
+    }
+}
