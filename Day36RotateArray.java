de20f685bf9ee7c8a6930cbfb74a0449da596664
Shashank-space <shashanksp02@gmail.com>
2026-07-27 23:51:24 +0530

    Day 36 - Rotate Array

--- Day36RotateArray.java
+++ Day36RotateArray.java
@@ -19,7 +19,7 @@ public class Day36RotateArray {
             right--;
         }
     }
-
+//
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
         int k = 3;
