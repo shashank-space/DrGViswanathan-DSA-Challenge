df4da536a2f591f1d8989eb6ab8ba50eaa556a2c
Shashank-space <shashanksp02@gmail.com>
2026-07-28 23:56:51 +0530

    day 38F - Find First and Last Position of Element in Sorted Array

---
+++ Day38FindFirstandLastPositionofElementinSortedArray.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+
+public class Day38FindFirstandLastPositionofElementinSortedArray {
+
+    public static int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return new int[]{first, last};
+    }
+
+    private static int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private static int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+
+        int[] result = searchRange(nums, target);
+
+        System.out.println("First and Last Position: " + Arrays.toString(result));
+    }
+}
