91163f0bdcef816e9523f7b7a5e7c0c0704a94d4
Shashank-space <shashanksp02@gmail.com>
2026-08-04 00:33:10 +0530

    Day 41 - Minimum Depth Of Binary Tree

---
+++ Day41MinimumDepthOfBinaryTree.java
@@ -0,0 +1,40 @@
+
+public class Day41MinimumDepthOfBinaryTree {
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null) {
+            return 1 + minDepth(root.right);
+        }
+
+        if (root.right == null) {
+            return 1 + minDepth(root.left);
+        }
+
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println("Minimum Depth: " + minDepth(root));
+    }
+}
+
+
