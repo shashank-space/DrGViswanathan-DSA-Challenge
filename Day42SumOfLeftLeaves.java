a3755fa63c3df95ea4fe79b1f807d811a932c51a
Shashank-space <shashanksp02@gmail.com>
2026-08-04 00:33:53 +0530

    Day 42 - Sum Of Left Leaves

---
+++ Day42SumOfLeftLeaves.java
@@ -0,0 +1,41 @@
+public class Day42SumOfLeftLeaves {
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int sum = 0;
+
+        if (root.left != null &&
+            root.left.left == null &&
+            root.left.right == null) {
+            sum += root.left.val;
+        }
+
+        sum += sumOfLeftLeaves(root.left);
+        sum += sumOfLeftLeaves(root.right);
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println("Sum of Left Leaves: " + sumOfLeftLeaves(root));
+    }
+}
+
