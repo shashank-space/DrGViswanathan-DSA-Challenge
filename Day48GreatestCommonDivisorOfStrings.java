14f6174f4dae5037137c80106553d0f9e1511772
Shashank-space <shashanksp02@gmail.com>
2026-08-08 23:04:09 +0530

    Day 48 - Greatest Common Divisor Of Strings

---
+++ Day48GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,28 @@
+public class Day48GreatestCommonDivisorOfStrings {
+
+    public static String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+
+        int gcdLength = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLength);
+    }
+
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        String str1 = "ABCABC";
+        String str2 = "ABC";
+
+        System.out.println("GCD of Strings: " + gcdOfStrings(str1, str2));
+    }
+}
+
