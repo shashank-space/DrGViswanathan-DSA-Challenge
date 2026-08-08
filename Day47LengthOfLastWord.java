2a2f094a6a00c19b3061acbb879391039443a297
Shashank-space <shashanksp02@gmail.com>
2026-08-08 23:02:37 +0530

    Day 47 - Length Of Last Word

---
+++ Day47LengthOfLastWord.java
@@ -0,0 +1,24 @@
+public class Day47LengthOfLastWord {
+
+    public static int lengthOfLastWord(String s) {
+        int length = 0;
+        int i = s.length() - 1;
+
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+
+    public static void main(String[] args) {
+        String s = "Hello World";
+
+        System.out.println("Length of Last Word: " + lengthOfLastWord(s));
+    }
+}
